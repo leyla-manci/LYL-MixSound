8d917914ef2acd10ab263480d33ee8d65ece68c4
Leyla Akmanci <leyla.manci@gmail.com>
2020-07-17 03:57:08 +0300

    First commit with love <3
    Signed-off-by: Leyla Akmanci <leyla.manci@gmail.com>

---
+++ mixsound/src/main/java/lyl/manci/mixsound/SoundType.kt
@@ -0,0 +1,16 @@
+package lyl.manci.mixsound
+
+
+/**       Code with ❤  ´• ل •`   ❤
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+▬     Created by Leyla Akmancı                ▬
+▬     ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬    ▬
+▬     leyla.manci@gmail.com                           ▬
+▬     ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬     ▬
+▬     17/07/2020 - 03:44        ▬
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+ */
+
+enum class SoundType {
+    Fast, Slow, Chipmunk, DarthVader
+}
---
+++ mixsound/src/main/java/lyl/manci/mixsound/Util.kt
@@ -0,0 +1,27 @@
+package lyl.manci.mixsound
+
+import android.content.Context
+import android.os.Build
+import android.speech.tts.TextToSpeech
+import android.widget.Toast
+
+
+/**       Code with ❤  ´• ل •`   ❤
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+▬     Created by Leyla Akmancı                ▬
+▬     ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬    ▬
+▬     leyla.manci@gmail.com                           ▬
+▬     ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬     ▬
+▬     17/07/2020 - 03:44        ▬
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+ */
+
+infix fun String.toast(context: Context?) = Toast.makeText(context, this, Toast.LENGTH_SHORT).show()
+
+@Suppress("DEPRECATION")
+fun TextToSpeech.speak() = when {
+    Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP -> {
+        this.speak(MixSound.recordSound, TextToSpeech.QUEUE_FLUSH, null, null)
+    }
+    else -> this.speak(MixSound.recordSound, TextToSpeech.QUEUE_FLUSH, null)
+}
